fa122d49afffef74578921028673187ffa2ee099
AndreiOmnom <belcargoru@gmail.com>
2022-04-01 21:07:33 +0300

    не заканчивает цикл

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -45,63 +45,44 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner1 = new Scanner(System.in);
-        Scanner scanner2 = new Scanner(System.in);
-        int chislo1 = scanner1.nextInt();
-        int chislo2 = scanner2.nextInt();
+        Scanner scan1 = new Scanner(System.in);
+        int chislo1 = scan1.nextInt();
+        int chislo2 = scan1.nextInt();
 
-
+       //  int lr  = String.valueOf(chislo2).length();
         int last1 = chislo1 % 10;
         int last2 = chislo2 % 10;
         chislo1 = chislo1 / 10;
         chislo2 = chislo2 / 10;
-
-
-        if (last2 > 2) ;
+        while (chislo2 > 0 ) ;
         {
-            int point = 0;
-            point = last1 + last2 - 2;
-            System.out.print(point);
-            last1 = 0;
-            last2 = 0;
-        }
+            if (last2 > 2); {
 
-        if (last2 <= 2) ;
-        {
-            int point = 0;
-            point = last1 + last2;
+                int point = 0;
+                point = last1 + last2 - 2;
+                System.out.print(point);
+                last1 = 0;
+                last2 = 0;
 
-            System.out.print(point);
-            last1 = 0;
-            last2 = 0;
+            }
+            if (last2 <= 2); {
+
+                int point = 0;
+                point = last1 + last2;
+
+                System.out.print(point);
+                last1 = 0;
+                last2 = 0;
+
+
+             }
 
         }
         chislo1 = chislo1 / 10;
         chislo2 = chislo2 / 10;
     }
 
+
 }
 
 
-          //  chislo1  =  chislo1 / 10;
-          //  chislo2   =  chislo2 / 10;
-          //  break;
-          //  }
-           // length = length - 1;
-       // }
-
-
-//*    int number  = 54734;
-//    int cifrmax = number % 10;
-//   int cifrmin =number % 10;
- //           number = number /10;
- //                   while ( number > 0) {
- //                   int cifrlast = number % 10;
-//                    if (cifrlast <  cifrmin) {
- //       cifrmin = cifrlast;
- //       }
- //       cifrmax = cifrlast;
- //       }
- //       number = number / 10;
-  //      }
- //*       System.out.println("min=" + cifrmin + " max=" +cifrmax);
